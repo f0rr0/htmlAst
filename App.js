dcd2fc766f30b03d76715d73d25cef0bd7c0d2f2
Sid <sid26@ucla.edu>
2017-07-06 11:52:44 +0530

    merge contexts

--- App.js
+++ App.js
@@ -10,17 +10,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  bold: {
-    fontWeight: 'bold'
-  },
-  italic: {
-    fontStyle: 'italic'
-  },
   paragraph: {
     marginBottom: 10
-  },
-  break: {
-    height: 20
   },
   container: {
     marginTop: 30,
@@ -37,22 +28,36 @@ const styles = StyleSheet.create({
 
 class Bold extends PureComponent {
   static childContextTypes = {
-    textStyle: PropTypes.number
+    textStyle: PropTypes.object
+  }
+
+  static contextTypes = {
+    textStyle: PropTypes.object
   }
 
   getChildContext = () => ({
-    textStyle: styles.bold
+    textStyle: {
+      ...this.context.textStyle,
+      fontWeight: 'bold'
+    }
   })
 
   render = () => <Text>{this.props.children}</Text>
 }
 class Italic extends PureComponent {
   static childContextTypes = {
-    textStyle: PropTypes.number
+    textStyle: PropTypes.object
+  }
+
+  static contextTypes = {
+    textStyle: PropTypes.object
   }
 
   getChildContext = () => ({
-    textStyle: styles.italic
+    textStyle: {
+      ...this.context.textStyle,
+      fontStyle: 'italic'
+    }
   })
 
   render = () => <Text>{this.props.children}</Text>
@@ -61,7 +66,7 @@ const Paragraph = ({ children }) => <Text style={styles.paragraph}>{children}</T
 const Break = () => <Text>{'\n'}</Text>
 const ContextAwareText = ({ children }, context) => <Text style={context.textStyle || {}}>{children}</Text>
 ContextAwareText.contextTypes = {
-  textStyle: PropTypes.number
+  textStyle: PropTypes.object
 }
 
 const convertToReact = tree => tree.map(node => {
